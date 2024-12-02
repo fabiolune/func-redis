c9cf4b85fe2333e73e41ebe90dc6640b6f76925f
fabiolune <fabiolune@gmail.com>
2024-12-02 10:22:22 +0100

    changed extension visibility

--- Func.Redis/Extensions/EitherLoggingExtensions.cs
+++ Func.Redis/Extensions/EitherLoggingExtensions.cs
@@ -4,9 +4,9 @@ using TinyFp.Extensions;
 namespace Func.Redis.Extensions;
 public static class EitherLoggingExtensions
 {
-    public static Either<Error, T> TeeLog<T>(this Either<Error, T> self, ILogger logger, string component) =>
+    internal static Either<Error, T> TeeLog<T>(this Either<Error, T> self, ILogger logger, string component) =>
         self.MapLeft(err => err.Tee(e => logger.LogError("{Component} raised an error with {Message}", component, e.Message)));
 
-    public static Task<Either<Error, T>> TeeLog<T>(this Task<Either<Error, T>> self, ILogger logger, string component) =>
+    internal static Task<Either<Error, T>> TeeLog<T>(this Task<Either<Error, T>> self, ILogger logger, string component) =>
         self.MapLeftAsync(err => err.Tee(e => logger.LogError("{Component} raised an error with {Message}", component, e.Message)));
 }

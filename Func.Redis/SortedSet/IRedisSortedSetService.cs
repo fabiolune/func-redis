77504c73ce1233d89dd0770691da1a988c14e27d
fabiolune <fabiolune@gmail.com>
2025-06-30 17:44:43 +0200

    added sorted sets with initial tests covering

---
+++ Func.Redis/SortedSet/IRedisSortedSetService.cs
@@ -0,0 +1,35 @@
+﻿namespace Func.Redis.SortedSet;
+
+public interface IRedisSortedSetService
+{
+    Either<Error, Unit> Add<T>(string key, IEnumerable<(T Value, double Score)> values);
+    Either<Error, Unit> Add<T>(string key, T value, double score);
+    Task<Either<Error, Unit>> AddAsync<T>(string key, IEnumerable<(T Value, double Score)> values);
+    Task<Either<Error, Unit>> AddAsync<T>(string key, T value, double score);
+    Either<Error, Unit> Decrement<T>(string key, T value, double score);
+    Task<Either<Error, Unit>> DecrementAsync<T>(string key, T value, double score);
+    Either<Error, Unit> Increment<T>(string key, T value, double score);
+    Task<Either<Error, Unit>> IncrementAsync<T>(string key, T value, double score);
+    Either<Error, T[]> Intersect<T>(string[] keys);
+    Task<Either<Error, T[]>> IntersectAsync<T>(string[] keys);
+    Either<Error, long> Length(string key);
+    Either<Error, long> Length(string key, double min, double max);
+    Task<Either<Error, long>> LengthAsync(string key);
+    Task<Either<Error, long>> LengthAsync(string key, double min, double max);
+    Either<Error, long> LengthByValue<T>(string key, T min, T max);
+    Task<Either<Error, long>> LengthByValueAsync<T>(string key, T min, T max);
+    Either<Error, Option<long>> Rank<T>(string key, T value);
+    Task<Either<Error, Option<long>>> RankAsync<T>(string key, T value);
+    Either<Error, Unit> Remove<T>(string key, IEnumerable<T> values);
+    Either<Error, Unit> Remove<T>(string key, T value);
+    Task<Either<Error, Unit>> RemoveAsync<T>(string key, IEnumerable<T> values);
+    Task<Either<Error, Unit>> RemoveAsync<T>(string key, T value);
+    Either<Error, Unit> RemoveRangeByScore(string key, double start, double stop);
+    Task<Either<Error, Unit>> RemoveRangeByScoreAsync(string key, double start, double stop);
+    Either<Error, Unit> RemoveRangeByValue<T>(string key, T min, T max);
+    Task<Either<Error, Unit>> RemoveRangeByValueAsync<T>(string key, T min, T max);
+    Either<Error, Option<double>> Score<T>(string key, T value);
+    Task<Either<Error, Option<double>>> ScoreAsync<T>(string key, T value);
+    Either<Error, T[]> Union<T>(string[] keys);
+    Task<Either<Error, T[]>> UnionAsync<T>(string[] keys);
+}
---
+++ tests/Func.Redis.Tests/SortedSet/RedisSortedSetService/RedisSortedSetServiceTests.Add.cs
@@ -0,0 +1,77 @@
+﻿using static Func.Redis.Tests.TestDataElements;
+
+namespace Func.Redis.Tests.SortedSet.RedisSortedSetService;
+
+internal partial class RedisSortedSetServiceTests
+{
+    [Test]
+    public void AddAsync_ShouldCallDbSortedSetAdd_WhenCalled()
+    {
+        var data = new TestData(1);
+        _mockSerDes
+            .Serialize(data)
+            .Returns((RedisValue)"serialized");
+
+        _mockDb
+            .SortedSetAdd("key", "serialized", 1)
+            .Returns(true);
+
+        var result = _sut.Add("key", data, 1);
+
+        result.IsRight.Should().BeTrue();
+    }
+
+    [Test]
+    public async Task AddAsync_WhenDatabaseReturnsTrue_ShouldReturnUnit()
+    {
+        var data = new TestData(1);
+        _mockSerDes
+            .Serialize(data)
+            .Returns((RedisValue)"serialized");
+
+        _mockDb
+            .SortedSetAddAsync("key", "serialized", 1)
+            .Returns(Task.FromResult(true));
+
+        var result = await _sut.AddAsync("key", data, 1);
+
+        result.IsRight.Should().BeTrue();
+    }
+
+    [Test]
+    public void Add_WhenDatabaseReturnsFalse_ShouldReturnRedisError()
+    {
+        var data = new TestData(1);
+        _mockSerDes
+            .Serialize(data)
+            .Returns((RedisValue)"serialized");
+
+        _mockDb
+            .SortedSetAdd("key", "serialized", 1)
+            .Returns(false);
+
+        var result = _sut.Add("key", data, 1);
+
+        result.IsLeft.Should().BeTrue();
+        result.OnLeft(e => e.Should().BeEquivalentTo(Error.New("Redis ZADD Error")));
+    }
+
+    [Test]
+    public async Task AddAsync_WhenDatabaseReturnsFalse_ShouldReturnRedisError()
+    {
+        var data = new TestData(1);
+        _mockSerDes
+            .Serialize(data)
+            .Returns((RedisValue)"serialized");
+
+        _mockDb
+            .SortedSetAddAsync("key", "serialized", 1)
+            .Returns(Task.FromResult(false));
+
+        var result = await _sut.AddAsync("key", data, 1);
+
+        result.IsLeft.Should().BeTrue();
+        result.OnLeft(e => e.Should().BeEquivalentTo(Error.New("Redis ZADD Error")));
+    }
+}
+
